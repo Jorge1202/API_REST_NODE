a3835a4bac835a6e22d7d62bab3c55d595b99b23
Jorge1202 <yorch.uaeh@gmail.com>
2022-06-01 15:38:40 -0500

    versionado en routes

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,13 @@ function routerApi(app) {
   router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
 
-  // //http://localhost:3000/api/v2/users
-  // //se puede generar unas rutas con diferentes versiones (es usado cuando trabajamos en el versionado)
-  // app.use('/api/v2', router);
-  // //aqui se genera las rutas
-  // app.use('/products', productsRouter);
-  // app.use('/users', usersRouter);
-  // app.use('/categories', categoriesRouter);
+  //http://localhost:3000/api/v2/users
+  //se puede generar unas rutas con diferentes versiones (es usado cuando trabajamos en el versionado)
+  // const routerUrl = express.Router();
+  // app.use('/api/v2', routerUrl);
+  // routerUrl.use('/products', productsRouter);
+  // routerUrl.use('/users', usersRouter);
+  // routerUrl.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;
-
-//https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-//https://http.cat
